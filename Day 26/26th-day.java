efa64761ae7aff76326e6fe6c3b20f4ca6f17316
daxx5020 <dakshmakwana2146@gmail.com>
2024-05-18 00:01:00 +0530

    added question in day 26

--- Day 26/26th-day.java
+++ Day 26/26th-day.java
@@ -1,8 +1,11 @@
 /*
 Task
-A prime is a natural number greater than 1 that has no positive divisors other than 1 and itself. Given a number. n. determine and print whether it is Prime or Not prime.
+Your local library needs your help! Given the expected and actual return dates for a library book. create a program that calculates the fine (if any). The fee structure is as follows:
 
-Note: If possible, try to come up with a O(Vn) primality algorithm, or see what sort of optimizations you come up with for an O(n) algorithm. Be sure to check out the Editorial after submitting your code.
+1. If the book is returned on or before the expected return date, no fine will be charged (i.e.: fine = 0).
+2. If the book is returned after the expected return day but still within the same calendar month anc year as the expected return date, fine = 15 Hackos × (the number of days late).
+3. If the book is returned after the expected return month but still within the same calendar year as the expected return date, the fine = 500 Hackos × (the number of months late).
+4. If the book is returned after the calendar year in which it was expected, there is a fixed fine of 10000 Hackos.
 */
 
 
