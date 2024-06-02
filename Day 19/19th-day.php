baf127e6789e6aa4b33ab4df0353ec8b676ba622
daxx5020 <dakshmakwana2146@gmail.com>
2024-06-02 22:56:09 +0530

    changed in day 19 question

--- Day 19/19th-day.php
+++ Day 19/19th-day.php
@@ -1,16 +1,8 @@
 <!-- 
 Task
-A palindrome is a word, phrase, number, or other sequence of characters which reads the same backwards and forwards. Can you determine if a given string, s, is a palindrome? To solve this challenge, we must first take each character in s, enqueue it in a queue, and also push that same character onto a stack. Once that's done, we must dequeue the first character from the queue and pop the top character off the stack, then compare the two characters to see if they are the same: as long as the characters match, we continue dequeueing, popping, and comparing each character until our containers are empty (a non-match means s isn't a palindrome).
+The AdvancedAr ithmetic interface and the method declaration for the abstract divisor Sum(n) method are provided for you in the editor below.
 
-Write the following declarations and implementations:
-1. Two instance variables: one for your stack, and one for your queue.
-2. A void pushCharacter(char ch) method that pushes a character onto a stack.
-3. A void enqueueCharacter(char ch) method that enqueues a character in the queue instance
-variable.
-4. A char popCharacter method that pops and returns the character at the top of the stack
-instance variable.
-5. A char dequeueCharacter method that dequeues and returns the first character in the queue
-instance variable.
+Complete the implementation of Calculator class, which implements the AdvancedAr ithmetic interface. The implementation for the divisorSum(n) method must return the sum of all divisors of n.
  -->
 
 
